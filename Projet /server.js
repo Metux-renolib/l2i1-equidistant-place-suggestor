909782ba67e253fc77ce477fad5a888b8513c936
Riadeuh <riadpugenger@hotmail.fr>
2022-03-09 13:51:57 +0100

    essai

--- Projet /server.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-const app=express()
-
-app.get("/api",(req,res) => {
-    res.json({"users":["Moncef","ceci est ","un test "]})
-}
-)
-
-app.listen(5000,() =>{ console.log("Server started at port 5000")})

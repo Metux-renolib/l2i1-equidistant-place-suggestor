dbbb399f8e9ba2187842a40918580d25254de7b5
Riadeuh <riadpugenger@hotmail.fr>
2022-03-17 09:33:45 +0100

    modifications rendu formulaire

--- front/src/components/Formulaire.js
+++ front/src/components/Formulaire.js
@@ -43,37 +43,49 @@ const  Formulaire = () => {
         <Navigation/>
         <form action="/" method="post">
             <label>Nom 1 :</label>
-            <input type='text' name ='name1' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name1' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 1  : </label>
             <input type='text' name ='adresse1' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
             <label>Nom 2 :</label>
-            <input type='text' name ='name2' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name2' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 2  : </label>
             <input type='text' name ='adresse2' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
             <label>Nom 3 :</label>
-            <input type='text' name ='name3' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name3' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 3  : </label>
             <input type='text' name ='adresse3' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
             <label>Nom 4 :</label>
-            <input type='text' name ='name4' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name4' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 4  : </label>
             <input type='text' name ='adresse4' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
             <label>Nom 5 :</label>
-            <input type='text' name ='name5' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name5' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 5  : </label>
             <input type='text' name ='adresse5' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
             <label>Nom 6 :</label>
-            <input type='text' name ='name6' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez votre nom'/>
+            <input type='text' name ='name6' onChange={(e) => { setNom(e.target.value);}} placeholder='Rentrez un nom'/>
             <label>Adresse 6  : </label>
             <input type='text' name ='adresse6' onChange={(e) => { setAdresse(e.target.value);}} placeholder='Rentrez une adresse'/>
 
+            <br/>
+
         </form>
-        <input type="submit" value="Envoyer" />
+        <input type="submit" value="Envoyer" onClick={handleSubmit} />
     </div>
   );
 }

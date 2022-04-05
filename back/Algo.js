76ae8d2084ed6fbc4b4bac88c981ea06fa630518
OrianeCrouzet <oriane.crouzet78@gmail.com>
2022-04-05 18:07:20 +0200

    Modifs Algo
    Suppression des console.log() qui avaient servis pour le debugge de l'algo

--- back/Algo.js
+++ back/Algo.js
@@ -36,19 +36,6 @@ function algo(tab) {
     }
     nbAdresses = tabAdresses.length; 
 
-    // let tabAdresses = new Array(); 
-    // tabAdresses[0] = new CoordonnesAdresse(48.8553052, 2.3318301);
-    // tabAdresses[1] = new CoordonnesAdresse(48.8913792, 2.2524573);
-    // tabAdresses[2] = new CoordonnesAdresse(49.012608, 1.913285);
-    // nbAdresses = 3; 
-
-    console.log("Adresse 1 : " + tabAdresses[0].lat + " " + tabAdresses[0].lng)
-    console.log(" ")
-    console.log("Adresse 2 : " + tabAdresses[1].lat + " " + tabAdresses[1].lng)
-    console.log(" ")
-    console.log("Adresse 3 : " + tabAdresses[2].lat + " " + tabAdresses[2].lng)
-    console.log(" ")
-
     //Créer tous les cercles passant par deux points
     //Calcul des diamètres de chaque cercle formé par les points deux à deux
     let tabDiametresCercles = new Array(); //Tableau contenant tous les diamètres de tous les cercles formés
@@ -65,18 +52,12 @@ function algo(tab) {
             tabCentresCercles.push(CentreDuCercle(tabAdresses[i].lat, tabAdresses[i].lng, tabAdresses[j].lat, tabAdresses[j].lng));
         }
     }
-    console.log("Centre cercle 1 : " + tabCentresCercles[0].lat + " " + tabCentresCercles[0].lng)
-    console.log(" ")
-    console.log(console.log("Centre cercle 2 : " + tabCentresCercles[1].lat + " " + tabCentresCercles[1].lng))
-    console.log(" ")
 
     //Calcul du rayon de chaque cercle
     let tabRayonsCercles = new Array(); //Tableau contenant les rayons de tous les cercles
     for (i = 0; i <= tabDiametresCercles.length - 1; i++) {
         tabRayonsCercles.push(Rayon(tabDiametresCercles[i]));
     }
-    console.log("Rayons cercles : " + tabRayonsCercles)
-    console.log(" ")
 
     //On compare alors les distances points/centre au rayon pour déterminer si le point se trouve dans le cercle ou non, pour chaque cercle
     let tabCerclesInteressants = new Array();
@@ -94,10 +75,6 @@ function algo(tab) {
             tabCentresInteressants.push(tabCentresCercles[i]); 
         }
     }
-    console.log("NB Cercles : " + tabCerclesInteressants.length)
-    console.log(" ")
-    console.log("NB Centres : " + tabCentresInteressants.length)
-    console.log(" ")
 
     //On veut trouver le plus petit rayon parmi tous nos cercles intéressants (dans les deux tableaux)
     let rayonMin = tabCerclesInteressants[0];
@@ -110,7 +87,6 @@ function algo(tab) {
             pointEquidistant = tabCentresInteressants[i];
         }
     }
-    console.log("Point equidistant : " + pointEquidistant.lat + " " + pointEquidistant.lng);
 
     let tabFinal = new Array();
     tabFinal = tabAdresses;
